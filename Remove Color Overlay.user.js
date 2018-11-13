ce4ac25e0a9927ab3a9b55a310e15098bf83eb7b
Chad Scott <chad.scott2@gmail.com>
2018-11-13 11:32:30 -0600

    Added event to window.onload
    the script wasn't firing during initial site visit, only on page refresh. Added window.onload event to make the script wait until the page is fully loaded before it fires.

--- Remove Color Overlay.user.js
+++ Remove Color Overlay.user.js
@@ -1,20 +1,25 @@
 // ==UserScript==
 // @name         Remove Color Overlay from Dashboard
-// @version      0.5
+// @version      1.0
 // @description  Removes the color overlay from course tiles with images
 // @author       Chad Scott
 // @match        https://*.instructure.com/
 // @grant        none
 // ==/UserScript==
 
-(function() {
+( function() {
     'use strict';
-
 //Remove color overlay from course images
-var x = document.getElementsByClassName('ic-DashboardCard__header_image');
+function rColor(){
+    var x = document.getElementsByClassName('ic-DashboardCard__header_image');
 var i;
 for (i = 0; i < x.length; i++) {
     var child = x[i].querySelector('.ic-DashboardCard__header_hero');
 child.style.opacity = "0";
 }
+}
+
+window.onload = function() {
+   rColor();
+};
 })();
