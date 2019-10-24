840668cb52f81c28185c621ea6820fc70d8400c8
Chad Scott <chad.scott2@gmail.com>
2019-10-24 09:30:26 -0500

    Update printCanvasQuizzes.user.js

--- printCanvasQuizzes.user.js
+++ printCanvasQuizzes.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Print Canvas Quiz
 // @namespace    https://github.com/sukotsuchido/CanvasUserScripts
-// @version      1.0
+// @version      1.2
 // @description  Allows the user to print quizzes from the preview page.
 // @author       Chad Scott (ChadScott@katyisd.org)
 // @include      https://*.instructure.com/courses/*/quizzes/*/take?preview*
+// @require      https://code.jquery.com/jquery-3.4.1.min.js
 
 // ==/UserScript==
 (function() {
